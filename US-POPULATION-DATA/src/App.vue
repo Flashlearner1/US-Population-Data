bf76778efd02a2a153b4bb6041b781838dbf54d6
Flashlearner1 <osayandekingsley768@gmail.com>
2023-07-25 15:30:37 -0700

    us population data

---
+++ US-POPULATION-DATA/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <USPOPULATIONDATA />
+  </div>
+</template>
+
+<script>
+import USPOPULATIONDATA from './components/USPOPULATIONDATA.vue';
+
+export default {
+  components: {
+    USPOPULATIONDATA
+  }
+};
+</script>
+
+<style>
+button {
+ padding: 12px 32px;
+ font-size: 16px;
+ border-radius: 8px;
+}
+
+.button:hover {
+  background-color: #fcbb08;
+  transition: .4s;
+}
+.data ul{
+  list-style-type: none;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 30px;
+}
+.table tr{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ US-POPULATION-DATA/src/components/USPOPULATIONDATA.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="data">
+    <h1>US POPULATION DATA</h1>
+    <button class="button" @click="fetchData">Fetch Data</button>
+    <br><br>
+    <table class="table" style="width:50%">
+  <tr>
+    <th>Nation</th>
+    <th>Population</th>
+    <th>Year</th>
+  </tr>
+
+  <tr class="row" v-for="item in data" :key="item.ID">
+    <td>{{ item.Nation }}</td>
+    <td>{{ item.Population }}</td>
+    <td>{{ item.Year }}</td>
+  </tr>
+  <tr class="row" v-for="item in data" :key="item.ID">
+    <td>{{ item.Nation }}</td>
+    <td>{{ item.Population }}</td>
+    <td>{{ item.Year }}</td>
+  </tr>
+
+</table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: []
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
+        const jsonData = await response.json();
+        this.data = jsonData.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+  }
+};
+</script>
+
